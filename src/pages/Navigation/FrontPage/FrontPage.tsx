8326a723abe1a815c7062f16fde1e333dddc8847
Johnny Davidsen Poulsen <jdp@mjolner.dk>
2021-11-28 17:59:24 +0100

    Added frontpage header

--- src/pages/Navigation/FrontPage/FrontPage.tsx
+++ src/pages/Navigation/FrontPage/FrontPage.tsx
@@ -1,5 +1,36 @@
+import { createStyles, TextInput } from '@mantine/core';
 import React, { FC } from 'react';
 
+const useStyles = createStyles((theme) => ({
+  root: {
+    display: 'grid',
+    gridTemplateRows: '300px auto',
+  },
+  searchHeader: {
+    paddingRight: theme.spacing.xl,
+    paddingLeft: theme.spacing.xl,
+    display: 'flex',
+    justifyContent: 'stretch',
+    alignItems: 'center',
+    height: 300,
+    backgroundImage: `url(${process.env.PUBLIC_URL}'/img/pexels-engin-akyurt-2725744.jpg')`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: '50% 50%',
+  },
+  searchField: { width: '100%' },
+}));
+
 export const FrontPage: FC = () => {
-  return <>Frontpage</>;
+  const { classes } = useStyles();
+  return (
+    <div className={classes.root}>
+      <div className={classes.searchHeader}>
+        <div className={classes.searchField}>
+          <TextInput variant="filled" size="lg" type="search" />
+        </div>
+      </div>
+      <div>Her kommer de seneste anmeldelser</div>
+    </div>
+  );
 };
